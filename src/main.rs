2f58883e055595f012e8c302fbd7999f0719640b
edamame-maru <so1maru.ueda@gmail.com>
2025-02-17 12:04:28 +0000

    added cmdline arguments

--- src/main.rs
+++ src/main.rs
@@ -1,67 +1,33 @@
-use std::io;
-use std::io::Write;
-
-const ITER: i32 = 100;
-const STEP: f32 = 0.000001;
-const STRT: f32 = 10.0;
-
-fn f(x: f32) -> f32 {
-    (x - 1.0) * (x - 2.0) * (x - 3.0)
-}
-fn findr(u: f32) -> f32 {
-    let mut x = u;
-    for _ in 0..ITER {
-        let m = (f(x + STEP) - f(x)) / STEP;
-        let c = f(x) - m * x;
-        x = -(c / m);
-    }
-
-    x
-}
+use std::env;
 
 fn main() {
-    print!("degree: ");
-    io::stdout().flush().unwrap();
-    let mut buffer = String::new();
-    io::stdin()
-        .read_line(&mut buffer)
-        .expect("failed to read line");
+    // easier to deal with cmdline arguments
+    let args: Vec<String> = env::args().collect();
+    let mut coeffs: Vec<f32> = vec![];
 
-    let degree: i32 = buffer.trim().parse().unwrap();
-
-    // user needs to see the polynomial with no coefficients
-    for i in 0..=degree {
-        if i == 0 {
-            print!("f(x) = ");
-        }
-
-        if i == degree {
-            println!("_");
-        } else {
-            print!("_x^{} + ", degree - i);
-        }
-    }
-
-    let mut coeffs: Vec<i32> = vec![];
-
-    for i in 0..=degree {
-        if i == degree {
-            print!("enter _ for constant term <_>: ");
-        } else {
-            print!("enter _ for term <_>x^{}: ", degree - i);
-        }
-        io::stdout().flush().unwrap();
-
-        buffer = String::new();
-        io::stdin()
-            .read_line(&mut buffer)
-            .expect("failed to read line");
-
-        let coeff: i32 = buffer.trim().parse().unwrap();
+    // coeffs must be f32 to deal with adding h
+    for i in 1..args.len() {
+        let coeff: f32 = args[i].trim().parse().unwrap();
         coeffs.push(coeff);
     }
 
-    println!("{:?}", coeffs)
+    println!("{:?}", coeffs);
 
-    //println!("f(x) = (x - 1)(x - 2)(x - 3):\n => f({}) = 0", findr(STRT));
+    // find roots
+    let mut x: f32 = 0.0;
+    for _ in 0..100000 {
+        let mut current: f32 = 0.0;
+        let mut forward: f32 = 0.0;
+        for i in 0..coeffs.len() {
+            current += coeffs[i] * x.powf((coeffs.len() - (i + 1)) as f32);
+            forward += coeffs[i] * (x + 0.000001).powf((coeffs.len() - (i + 1)) as f32);
+        }
+        let m = (forward - current) / 0.000001;
+
+
+        let c = current - m * x;
+        x = -(c / m);
+    }
+
+    println!("{}", x);
 }
